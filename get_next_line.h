62158c6876f0f324967b4a0e4f1650a72e2a6117
khasey <92310470+khasey@users.noreply.github.com>
2022-11-27 23:58:45 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+char *get_nest_line(int fd);
+
+#define BUFFER_SIZE 100
+
+#endif
